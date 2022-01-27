8e212dd82ef36d93b54683205fc4c7c84b0307d4
tyscurtis25 <73607580+tyscurtis25@users.noreply.github.com>
2022-01-26 23:49:17 -0700

    Add project files.

---
+++ DateMe-Mission4/Controllers/HomeController.cs
@@ -0,0 +1,49 @@
+﻿using DateMe_Mission4.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DateMe_Mission4.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Baconsale()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult MoviesForm()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult MoviesForm(Movies response)
+        {
+            return View("ConfirmationPage", response);
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ DateMe-Mission4/Models/Movies.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace DateMe_Mission4.Models
+{
+    public class Movies
+    {
+        [Key]
+        [Required]
+        public int MovieID { get; set; }
+
+        [Required]
+        public string Category { get; set; }
+
+        [Required]
+        public string Title { get; set; }
+
+        [Required]
+        [Range(1900, 2050, ErrorMessage ="Must be later than 1900")]
+        public int Year { get; set; }
+
+        [Required]
+        public string Director { get; set; }
+
+        [Required]
+        public string Rating { get; set; }
+        public bool Edited { get; set; }
+        public string Lent_to { get; set; }
+
+        [StringLength(25, ErrorMessage ="Notes are limited to 25 characters.")]
+        public int Notes { get; set; }
+
+    }
+}
